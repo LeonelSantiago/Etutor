f3df19dc4dd8be47d4d681749e8dc8285d6daa68
Leonel Antonio Santiago Baez <santiagobaez@msn.com>
2020-07-12 19:39:28 -0400

    #10 Add of EventsTypes entity

---
+++ Etutor/Etutor.DataModel/Entities/EventsTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etutor.DataModel.Entities
+{
+    public class EventsTypes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsGlobal { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
