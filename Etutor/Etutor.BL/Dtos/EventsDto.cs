f9dc3b64c9bc4c7697ed481cde8be6613696ac87
Leonel Antonio Santiago Baez <santiagobaez@msn.com>
2020-07-13 00:35:52 -0400

    #10 Add of Events and EventsTypes Dto's

---
+++ Etutor/Etutor.BL/Dtos/EventsDto.cs
@@ -0,0 +1,28 @@
+﻿using Etutor.BL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etutor.BL.Dtos
+{
+    public class EventsDto : IEntityBaseDto
+    {
+        #region Ctor
+        public EventsDto()
+        {
+
+        }
+
+        #endregion
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int EventTypeId { get; set; }
+        public virtual EventsDto EventsTypes { get; set; }
+    }
+}
---
+++ Etutor/Etutor.BL/Dtos/EventsTypesDto.cs
@@ -0,0 +1,15 @@
+﻿using Etutor.BL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etutor.BL.Dtos
+{
+    public class EventsTypesDto : IEntityBaseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsGlobal { get; set; }
+    }
+}
